55288f61fde95d3f00a3f4e1b9b42696ba183789
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2023-10-01 00:32:42 +0600

    Update file.lua

--- file.lua
+++ file.lua
@@ -9,6 +9,13 @@ function UpdateTime(menu)
     menu.Text = os.date("%H:%M:%S")
 end
 
+local ui = {}
+
+function ui:ChangeFont(Font)
+    local font = Font
+end
+
+
 local frame2 = Instance.new("TextLabel", frame)
 frame2.Size = UDim2.new(1, 0, 0, 32)
 frame2.BorderSizePixel = 0
@@ -18,7 +25,7 @@ frame2.BackgroundColor3 = Color3.fromRGB(0,0,0)
 frame2.BackgroundTransparency = 0.5
 frame2.TextColor3 = Color3.fromRGB(255,255,255)
 frame2.Text = os.date("%H:%M:%S")
-frame2.Font = "GothamMedium"
+frame2.Font = font or "GothamMedium"
 frame2.TextSize = 15
 
 local anim = TweenService:Create(frame2, TweenInfo.new(0.5), {Position = UDim2.new(0, -10, 1, 0)})
@@ -27,6 +34,8 @@ anim:Play()
 
 local uicorner = Instance.new("UICorner", frame2)
 uicorner.CornerRadius = UDim.new(0,8)
+
+
 
 while wait(1) do
     UpdateTime(frame2)
