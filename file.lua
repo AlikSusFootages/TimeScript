510e3b67b81093d201ae00752e3f4ab5671eeb37
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2023-09-30 21:26:36 +0600

    Update file.lua

--- file.lua
+++ file.lua
@@ -18,7 +18,7 @@ frame2.BackgroundColor3 = Color3.fromRGB(0,0,0)
 frame2.BackgroundTransparency = 0.5
 frame2.TextColor3 = Color3.fromRGB(255,255,255)
 frame2.Text = os.date("%H:%M:%S")
-frame2.Font = "Arial"
+frame2.Font = "Code"
 frame2.TextSize = 15
 
 local anim = TweenService:Create(frame2, TweenInfo.new(0.5), {Position = UDim2.new(0, -10, 1, 0)})
