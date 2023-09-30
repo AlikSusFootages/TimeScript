750fa00c968a854154d900f7975406f694672fae
ftgs <129105531+AlikSusFootages@users.noreply.github.com>
2023-10-01 00:41:43 +0600

    Update file.lua

--- file.lua
+++ file.lua
@@ -11,32 +11,35 @@ end
 
 local ui = {}
 
-function ui:ChangeFont(Font)
-    local font = Font or "GothamMedium"
+function ui:CreateTimer
+    local fonntt = {}
+    function fonntt:ChangeFont(Font)
+        local font = Font or "GothamMedium"
+    end
+
+
+    local frame2 = Instance.new("TextLabel", frame)
+    frame2.Size = UDim2.new(1, 0, 0, 32)
+    frame2.BorderSizePixel = 0
+    frame2.AnchorPoint = Vector2.new(0, 1)
+    frame2.Position = UDim2.new(0,-10,0,0)
+    frame2.BackgroundColor3 = Color3.fromRGB(0,0,0)
+    frame2.BackgroundTransparency = 0.5
+    frame2.TextColor3 = Color3.fromRGB(255,255,255)
+    frame2.Text = os.date("%H:%M:%S")
+    frame2.Font = font
+    frame2.TextSize = 15
+
+    local anim = TweenService:Create(frame2, TweenInfo.new(0.5), {Position = UDim2.new(0, -10, 1, 0)})
+
+    anim:Play()
+
+    local uicorner = Instance.new("UICorner", frame2)
+    uicorner.CornerRadius = UDim.new(0,8)
+
+    while wait(1) do
+        UpdateTime(frame2)
+    end
+    return font
 end
-
-
-local frame2 = Instance.new("TextLabel", frame)
-frame2.Size = UDim2.new(1, 0, 0, 32)
-frame2.BorderSizePixel = 0
-frame2.AnchorPoint = Vector2.new(0, 1)
-frame2.Position = UDim2.new(0,-10,0,0)
-frame2.BackgroundColor3 = Color3.fromRGB(0,0,0)
-frame2.BackgroundTransparency = 0.5
-frame2.TextColor3 = Color3.fromRGB(255,255,255)
-frame2.Text = os.date("%H:%M:%S")
-frame2.Font = font
-frame2.TextSize = 15
-
-local anim = TweenService:Create(frame2, TweenInfo.new(0.5), {Position = UDim2.new(0, -10, 1, 0)})
-
-anim:Play()
-
-local uicorner = Instance.new("UICorner", frame2)
-uicorner.CornerRadius = UDim.new(0,8)
-
-
-return ui 
-while wait(1) do
-    UpdateTime(frame2)
-end
+return ui
